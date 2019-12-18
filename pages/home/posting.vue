d4590840eb69baae0f8bb40eae2395db18266aed
Clarck <zhiwei.zhou@ydcfo.com.cn>
2019-12-18 13:58:18 +0800

    11

--- pages/home/posting.vue
+++ pages/home/posting.vue
@@ -81,7 +81,7 @@ export default {
     choosePic() {
       let self = this;
       uni.chooseImage({
-        count: 1,
+        count: 9,
         sizeType: ["compressed"], // 可以指定是原图还是压缩图，默认二者都有
         sourceType: ["album", "camera"], // 可以指定来源是相册还是相机，默认二者都有
         success: function(res) {
